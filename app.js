ccb5b79287165277b4ab4bf0af18983dbe0dcaf5
Brecht Opstaele <brecht.opstaele@hotmail.com>
2020-12-05 15:44:11 +0100

    Aanpassingen gedaan voor uitvoering op Heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Lien:EGnwqZDlakiWu8Hc@cluster0.ut43h.mongodb.net/Eetfestijn?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Lien:EGnwqZDlakiWu8Hc@cluster0.ut43h.mongodb.net/Eetfestijn?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
